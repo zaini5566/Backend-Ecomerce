2c54007fa093adb8cdd5eac3574a33c56ea04f8c
zaini5566 <zain7865566@gmail.com>
2024-09-07 17:17:45 +0500

    Adress add

--- index.js
+++ index.js
@@ -313,7 +313,7 @@ const OrderSchema = new mongoose.Schema({
         firstName: { type: String, required: true },
         lastName: { type: String},
         email: { type: String, required: true }, 
-        street: { type: String, required: true },
+        Address: { type: String, required: true },
         city: { type: String, required: true },
         state: { type: String, required: true },
         zipCode: { type: String, required: true },
