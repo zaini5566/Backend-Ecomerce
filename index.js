ee2d3778728d112f20f232b0f98b177f21a26f57
zaini5566 <zain7865566@gmail.com>
2024-09-07 16:42:42 +0500

    Last Name

--- index.js
+++ index.js
@@ -311,8 +311,8 @@ const OrderSchema = new mongoose.Schema({
     }],
     deliveryInfo: {
         firstName: { type: String, required: true },
-        lastName: { type: String, required: true },
-        email: { type: String, required: true },
+        lastName: { type: String},
+        email: { type: String, required: true }, 
         street: { type: String, required: true },
         city: { type: String, required: true },
         state: { type: String, required: true },
